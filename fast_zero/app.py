3271de418a651c2e781f1c85264bb9fb27487b5c
James Miranda <james.pontesmiranda@cea.fr>
2025-05-16 13:03:55 +0200

    Create standard CRUD endpoints

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
@@ -19,3 +19,33 @@ def create_user(user: UserSchema):
     new_user = UserDB(**user.model_dump(), id=len(database) + 1)
     database.append(new_user)
     return new_user
+
+
+@app.get('/users/')
+def read_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
